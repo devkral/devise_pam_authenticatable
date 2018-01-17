243eb78a80971e248f5309f67f6a8099d5d9a436
alex <devkral@web.de>
2018-01-17 15:38:33 +0100

    remove password_required?, can clash, rename functions for beeing more compatible, needs new version

--- lib/devise_pam_authenticatable/model.rb
+++ lib/devise_pam_authenticatable/model.rb
@@ -4,12 +4,12 @@ module Devise
   module Models
     module PamAuthenticatable
 
-      def get_service
+      def get_pam_service
         return self.class.pam_service if self.class.instance_variable_defined?('@pam_service')
         ::Devise.pam_default_service
       end
 
-      def get_suffix
+      def get_pam_suffix
         return self.class.pam_suffix if self.class.instance_variable_defined?('@pam_suffix')
         ::Devise.pam_default_suffix
       end
@@ -29,24 +29,19 @@ module Devise
 
       def is_pam_account?
         return false unless get_pam_name
-        Rpam2.account(get_service, get_pam_name)
+        Rpam2.account(get_pam_service, get_pam_name)
       end
 
       def pam_setup(attributes)
         return unless ::Devise.emailfield && ::Devise.usernamefield
-        self[::Devise.emailfield] = Rpam2.getenv(get_service, get_pam_name, attributes[:password], 'email', false)
+        self[::Devise.emailfield] = Rpam2.getenv(get_pam_service, get_pam_name, attributes[:password], 'email', false)
         self[::Devise.emailfield] = attributes[::Devise.emailfield] if self[::Devise.emailfield].nil?
-        self[::Devise.emailfield] = "#{self[::Devise.usernamefield]}@#{get_suffix}" if self[::Devise.emailfield].nil? && get_suffix
-      end
-
-      def password_required?
-        false
+        self[::Devise.emailfield] = "#{self[::Devise.usernamefield]}@#{get_pam_suffix}" if self[::Devise.emailfield].nil? && get_pam_suffix
       end
 
       def get_pam_name
         return self[::Devise.usernamefield] if ::Devise.usernamefield && self[::Devise.usernamefield]
-        suffix = get_suffix
-        return nil unless suffix && ::Devise.emailfield
+        return nil unless ::Devise.emailfield && (suffix = get_pam_suffix)
         email = "#{self[::Devise.emailfield]}\n"
         pos = email.index("@#{suffix}\n")
         return nil unless pos
@@ -56,7 +51,7 @@ module Devise
       # Checks if a resource is valid upon authentication.
       def valid_pam_authentication?(pw)
         return nil unless get_pam_name
-        Rpam2.auth(get_service, get_pam_name, pw)
+        Rpam2.auth(get_pam_service, get_pam_name, pw)
       end
 
       module ClassMethods
