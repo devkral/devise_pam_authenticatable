f362d1c35e5b3d58a114188516165b9bb024b280
alex <devkral@web.de>
2018-01-17 15:50:49 +0100

    reorganize code, add comments

--- lib/devise_pam_authenticatable/model.rb
+++ lib/devise_pam_authenticatable/model.rb
@@ -14,11 +14,18 @@ module Devise
         ::Devise.pam_default_suffix
       end
 
-      def pam_conflict(_attributes)
-        # solve conflict between other and pam related user accounts
-        # to disable login with pam return nil elsewise return a (different?) user object
-        # as default assume there is never a conflict and return user object unchanged
-        self
+      def get_pam_name
+        return self[::Devise.usernamefield] if ::Devise.usernamefield && self[::Devise.usernamefield]
+        return nil unless ::Devise.emailfield && (suffix = get_pam_suffix)
+        email = "#{self[::Devise.emailfield]}\n"
+        pos = email.index("@#{suffix}\n")
+        return nil unless pos
+        email.slice(0, pos)
+      end
+
+      def is_pam_account?
+        return false unless get_pam_name
+        Rpam2.account(get_pam_service, get_pam_name)
       end
 
       def pam_conflict?
@@ -27,9 +34,11 @@ module Devise
         resource.respond_to?('password') && resource.password.present? && is_pam_account?
       end
 
-      def is_pam_account?
-        return false unless get_pam_name
-        Rpam2.account(get_pam_service, get_pam_name)
+      def pam_conflict(_attributes)
+        # solve conflict between other and pam related user accounts
+        # to disable login with pam return nil elsewise return a (different?) user object
+        # as default assume the conflict ok and return user object unchanged
+        self
       end
 
       def pam_setup(attributes)
@@ -37,15 +46,6 @@ module Devise
         self[::Devise.emailfield] = Rpam2.getenv(get_pam_service, get_pam_name, attributes[:password], 'email', false)
         self[::Devise.emailfield] = attributes[::Devise.emailfield] if self[::Devise.emailfield].nil?
         self[::Devise.emailfield] = "#{self[::Devise.usernamefield]}@#{get_pam_suffix}" if self[::Devise.emailfield].nil? && get_pam_suffix
-      end
-
-      def get_pam_name
-        return self[::Devise.usernamefield] if ::Devise.usernamefield && self[::Devise.usernamefield]
-        return nil unless ::Devise.emailfield && (suffix = get_pam_suffix)
-        email = "#{self[::Devise.emailfield]}\n"
-        pos = email.index("@#{suffix}\n")
-        return nil unless pos
-        email.slice(0, pos)
       end
 
       # Checks if a resource is valid upon authentication.
